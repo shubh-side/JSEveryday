61b7f7c447aa2ab84101f589637890e19b1e84ec
shubh-side <chaudharyshubham1000@gmail.com>
2024-06-30 19:30:55 +0530

    Different variables

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-/*ar a;
- function fn(){
-        
-    let b;
-    //const c;
-   
-    console.log(a);
+/*  Different variable types */
 
-    console.log(b);
-   // console.log(c);
- }
+var a = 5; 
+// var functionally or globally scope.
+// Can be redeclared.
+// Can be declared without initialization.
+// Can be updated.
+let b = 10; 
+// let is block scoped.
+// Cannot be re-declared within its scope.
+// Can be decalred without initialization.
+// Can be updated.
+const c = 15; 
+// const is block scoped.
+// Cannot be re-declared within its scope.
+// Must be initialized at the time of declaration.
+// Can never be updated.
 
- fn();
 
- */
 
- let a = 5;
- let b = '5';
- //console.log(a == b);
- //console.log(a === b);
 
-var c = [1,2,3,4];
 
-const arr = [a, b, c];
-console.log(arr);
+
+
+
+
 
