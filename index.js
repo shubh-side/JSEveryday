ba6920e397ae99fcd3dde7aa630f8e5b25cadbac
shubh-side <chaudharyshubham1000@gmail.com>
2024-06-30 19:09:16 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/*ar a;
+ function fn(){
+        
+    let b;
+    //const c;
+   
+    console.log(a);
+
+    console.log(b);
+   // console.log(c);
+ }
+
+ fn();
+
+ */
+
+ let a = 5;
+ let b = '5';
+ //console.log(a == b);
+ //console.log(a === b);
+
+var c = [1,2,3,4];
+
+const arr = [a, b, c];
+console.log(arr);
+
